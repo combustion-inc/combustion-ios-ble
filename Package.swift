ead2749599c7a6f96e312b147a06aef541c505fb
Jason Machacek <jmachacek@gmail.com>
2021-12-03 08:41:18 -0800

    Updated naming of internal contents of package.

--- Package.swift
+++ Package.swift
@@ -8,12 +8,6 @@ let package = Package(
     platforms: [
         .iOS(.v13)
     ],
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "CombustionBLE",
-            targets: ["combustion-ios-ble"]),
-    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/apple/swift-collections", "1.0.0"..<"2.0.0")
@@ -22,10 +16,11 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "combustion-ios-ble",
+            name: "CombustionBLE",
             dependencies: [
                 .product(name: "Collections", package: "swift-collections", condition: nil)
-            ]),
+            ],
+            path: "Sources/CombustionBLE"),
         /*
         .testTarget(
             name: "combustion-ios-bleTests",

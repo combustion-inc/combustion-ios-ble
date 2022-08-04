bb523ac75d21337d19af37c90f508ba7040eb504
Jesse Johnston <jesse.johnston@gmail.com>
2022-08-03 17:02:00 -0700

    Decode UART CRC

---
+++ Sources/CombustionBLE/Utilities/Data+CRC.swift
@@ -0,0 +1,46 @@
+//  Data+CRC.swift
+
+/*--
+MIT License
+
+Copyright (c) 2022 Combustion Inc.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+--*/
+
+import Foundation
+
+extension Data {
+    func crc16ccitt() -> UInt16 {
+        var crc: UInt16 = 0xFFFF // initial value
+        let polynomial: UInt16 = 0x1021 // 0001 0000 0010 0001  (0, 5, 12)
+
+        self.forEach { (byte) in
+            for i in 0...7 {
+                let bit = (byte >> (7 - i) & 1) == 1
+                let c15 = (crc  >> (15)    & 1) == 1
+                crc = crc << 1
+                if (c15 != bit) {
+                    crc = crc ^ polynomial
+                }
+            }
+        }
+        return crc & 0xffff
+    }
+}

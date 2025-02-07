16c1c7dfb65ee5ca05b954fce5f2c8bfb0027ff3
JustMeDevin <justmedevin@gmail.com>
2025-02-07 14:58:21 +1300

    Fix for missing switch cases on handlers

--- Sources/CombustionBLE/UART/MeatNet/NodeResponse.swift
+++ Sources/CombustionBLE/UART/MeatNet/NodeResponse.swift
@@ -152,6 +152,8 @@ extension NodeResponse {
             return NodeReadModelInfoResponse.fromRaw(data: data, success: success, requestId: requestId, responseId: responseId, payloadLength: Int(payloadLength))
         case .getFeatureFlags:
             return NodeReadFeatureFlagsResponse.fromRaw(data: data, success: success, requestId: requestId, responseId: responseId, payloadLength: Int(payloadLength))
+        case .resetSession:
+            return NodeResetSessionResponse(success: success, requestId: requestId, responseId: responseId, payloadLength: Int(payloadLength))
         case .custom(let address):
             return NodeCustomResponse(data: data, success: success, requestId: requestId, responseId: responseId, payloadLength: Int(payloadLength), messageType: messageType)
 //        case .readOverTemperature:

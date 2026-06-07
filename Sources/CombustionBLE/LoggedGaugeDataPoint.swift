34502f55f6a07ad20febb61d8defcc670a64b7c6
JustMeDevin <justmedevin@gmail.com>
2026-06-08 10:31:19 +1200

    Updated LoggedDeviceDataPointType to be iterable.

--- Sources/CombustionBLE/LoggedGaugeDataPoint.swift
+++ Sources/CombustionBLE/LoggedGaugeDataPoint.swift
@@ -23,7 +23,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 --*/
 
-public enum LoggedDeviceDataPointType: String, Codable {
+public enum LoggedDeviceDataPointType: String, Codable, CaseIterable {
      case probe
      case gauge
      case engine
